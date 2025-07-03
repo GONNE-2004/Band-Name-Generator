9a646e20b861a34109a3b8635f9c01acbcd80dad
GONNE-2004 <gonne411@gmail.com>
2025-07-03 02:15:21 +0200

    Simple band name generator-learnt concatention

---
+++ task/task.py
@@ -0,0 +1,4 @@
+print("WELCOME THE BAND NAME GENERATOR!")
+city_name = input("Enter the name of the city you grew up in. \n")
+pet_name = input("Enter the name of your Pet. \n")
+print("The name of your band is:" + city_name + " " + pet_name)
